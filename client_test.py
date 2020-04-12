a547735e7de0fdeaf4a2e3ae89e2bf7f0c6c7196
Nick <figginsn@gmail.com>
2020-04-12 11:36:39 -0400

    Create patch file

--- client_test.py
+++ client_test.py
@@ -8,6 +8,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    dataPoint = ('ABC', 120.48, 121.2, (120.48+121.2)/2)
+    self.assertEqual(getDataPoint(quotes[0]), dataPoint)
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,7 +17,9 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    # Data Point is of the form (stock, top_bid price, top_ask price, price )
+    dataPoint = ('ABC', 120.48, 119.2, (120.48+119.2)/2)
+    self.assertEqual(getDataPoint(quotes[0]), dataPoint)
 
   """ ------------ Add more unit tests ------------ """
 
